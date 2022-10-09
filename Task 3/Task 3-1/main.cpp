87adc1ec07e2014be8f06101f6e34e716d46c466
darkis <itchanebodi@gmail.com>
2022-10-09 21:46:17 +0300

    Начало работы

---
+++ Task 3/Task 3-1/main.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+using namespace std;
+
+/**
+* \brief Существует ли функция в точке х.
+* \param x Аргумент функции.
+* \ return true,  если существует.
+*/
+
+bool isExists(const double x);
+
+/**
+* \brief Расчёт функции в  точке х.
+* \param x Аргумент функции.
+* \return Значение функции в точке х.
+*/
+
+double getY(const double x);
+
+/**
+* \brief Точка входа в программу в  точке х.
+* \return Возвращает 0 случае успеха
+*/
+
+int main()
+{
+	const double xStart = 1;
+	const double xFinish = 2;
+	const double step = 0.1;
+	double x = xStart;
+
+	while (x <= xFinish)
+	{
+
+	}
+
+	return 0;
+}
+
+bool isExists(const double x)
+{
+
+}
+
+double Function(const double x)
+{
+
+}
