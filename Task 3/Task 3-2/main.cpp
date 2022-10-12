127eef029c59e0f6d14a4c8fc2e3979e804d3fef
darkis <itchanebodi@gmail.com>
2022-10-12 22:19:31 +0300

    Начало программы задание 3-2 #23

--- Task 3/Task 3-2/main.cpp
+++ Task 3/Task 3-2/main.cpp
@@ -1,13 +1,69 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
 /**
-*\brief Точка входа в программу
-\return Возвращает 0 случае успеха
+* \brief Считать количество членов последовательности.
+* \param message Сообщение пользователю.
+* \return Количество членов последовательности.
+*/
+
+size_t getCount(const string& message = "");
+
+/**
+* \brief Рассчитывает сумму последовательности.
+* \param count Количество членов последовательности.
+* \return Сумма последовательности.
+*/
+
+double getSum(const size_t count);
+
+/**
+* \brief Рассчитывает рекуррентный член последовательности.
+* \param k Количество членов последовательности.
+* \return Значение рекуррентного член последовательности.
+*/
+
+double getRecurrent(const size_t k);
+
+/**
+* \brief Точка входа в программу
+* \return Возвращает 0 случае успеха
 */
 
 int main()
 {
+	const auto count = ("Введите количество членов последовательности");
+	const double sum = getSum(count);
+	cout << " " << sum << "\n";
+
 	return 0;
+}
+
+size_t getCount(const string& message = "")
+{
+	cout << message;
+	int count;
+	cin >> count;
+	return count;
+}
+
+double getSum(const size_t count)
+{
+	double current = 1;
+	double sum = current;
+
+	for (size_t k = 2; k <= count; k++)
+	{
+		current *= getRecurrent(k);
+		sum += current;
+	}
+
+	return 0.0;
+}
+
+double getRecurrent(const size_t k)
+{
+	return (k + 1) * (k + 1) * (k + 1) / pow(k, 4);
 }
