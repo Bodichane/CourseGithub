f5ac65f914d5a7e63dbbb132ab2277af130b447f
darkis <113136403+Bodichane@users.noreply.github.com>
2022-10-30 22:42:30 +0300

    Update main.cpp

--- Task 3/Task 3-3/main.cpp
+++ Task 3/Task 3-3/main.cpp
@@ -16,10 +16,29 @@ double getY(const double x);
 /**
 * \brief Расчёт сумма в  точке х.
 * \param x Аргумент сумма.
+* \param epsilon Погрешность вычислений.
 * \return Значение сумма в точке х.
 */
 
-double getSum(const double x);
+double getSum(const double x, const double epsilon);
+
+/**
+* \brief Рассчитывает рекуррентный член последовательности.
+* \param n Количество членов последовательности.
+* \return Значение рекуррентного член последовательности.
+*/
+
+double getRecurrent(const double x);
+
+/**
+ * \brief Форматный вывод в поток вывода out (по умолчанию std::cout).
+ * \param argument Аргумент табулируемой функции.
+ * \param function Значение табулируемой функции.
+ * \param series Аппроксимация табулируемой функции.
+ * \param out Поток вывода, в который будет осуществлен форматный вывод.
+ */
+ 
+void FormatOutput(const double argument, const double function, const double sum, ostream& out = cout);
 
 /**
 * \brief Точка входа в программу в  точке х.
@@ -30,16 +49,17 @@ int main()
 {
 	const double xStart = 0.1;
 	const double xFinish = 0.8;
-	const double step = 0.0003;
-	double x = xStart;
-
-	cout << setw(10) << " x " << " " << " y " << " " << "Sum" << "\n";
-
-	while (x <= xFinish)
+	const double epsilon = 0.0003;
+	const double step = 0.01;
+	
+    double x = xStart;
+	while (x <= xFinish + epsilon)
 	{
 		const double y = getY(x);
-		const double sum = getSum(x);
-		cout << setw(10) << x << "  " << y << "  " << sum << "\n";
+		const double sum = getSum(x, epsilon);
+	
+	    FormatOutput(x, y, sum);
+
 		x += step;
 	}
 
@@ -48,10 +68,33 @@ int main()
 
 double getY(const double x)
 {
-	return 1 / 4 * log(1 + x / 1 - x) + 1 / 2 * atan(x);
+	return 1.0 / 4 * log(1 + x / 1 - x) + 1.0 / 2 * atan(x);
 }
 
-double getSum(const double x)
+double getSum(const double x, const double epsilon)
 {
-	return 1 / 1 - x;
+	auto previous = 0.0;
+    auto current = 1.0;
+    auto sum = current;
+
+    do
+    {
+        previous = current;
+        current = previous * getRecurrent(x);
+        sum += current;
+    } while (abs(previous - current) >= epsilon);
+
+    return sum;
+}
+
+double getRecurrent(const double x)
+{
+    return pow(5, x) / 5;
+}
+
+void FormatOutput(const double argument, const double function, const double sum, ostream& out)
+{
+    out << setw(10) << argument << " | "
+        << setw(15) << function << " | "
+        << setw(15) << sum << "\n";
 }
